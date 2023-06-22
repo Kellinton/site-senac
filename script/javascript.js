e12a26ea36893f5388ab6375f45bcdcb275eedc4
Kellinton <kellintonbrasil@gmail.com>
2023-06-22 16:05:04 -0300

    update

--- script/javascript.js
+++ script/javascript.js
@@ -15,3 +15,19 @@ $('.servicos-carro').slick({
   autoplay: true,
   autoplaySpeed: 2000,
 });
+
+window.onscroll = function(){
+
+  var topo = window.scrollY || document.documentElement.scrollTop;
+
+  if(topo > 140){
+    console.log("Adicionar menu fixo" + topo);
+    document.getElementById("topoFixo").classList.add("menu-fixo");
+    document.getElementById("topoFixo").classList.remove("site");
+  }
+  else{
+    console.log ("Remover menu fixo");
+    document.getElementById("topoFixo").classList.remove("menu-fixo");
+    document.getElementById("topoFixo").classList.add("site");
+  }
+}
