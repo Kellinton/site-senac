c3af154b88c6ce5c2cc2c07c0348574db031edc2
Kellinton <kellintonbrasil@gmail.com>
2023-07-01 20:23:08 -0300

    clientes responsivos

--- script/javascript.js
+++ script/javascript.js
@@ -6,7 +6,43 @@ window.alert('Formulário sendo feito');
 
 // inicializador do AOS
 AOS.init();
-
+$('.clientesCarro').slick({
+  dots: true,
+  infinite: true,
+  infinite: true,
+  autoplay: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 1,
