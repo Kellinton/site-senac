22b3cdac3361c4d9dda7084bfaba6855aa2a84c1
Kellinton <kellintonbrasil@gmail.com>
2023-07-03 17:20:47 -0300

    responsivo 50%
    carrousel funcionando

--- script/javascript.js
+++ script/javascript.js
@@ -51,11 +51,36 @@ $('.multiple-items').slick({
   autoplay: true,
 });
 
-$('.servicos-carro').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
+$('.servicosCarro').slick({
+  dots: true,
+  infinite: true,
+  infinite: true,
   autoplay: true,
-  autoplaySpeed: 2000,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 //#endregion Carroseul//
 
